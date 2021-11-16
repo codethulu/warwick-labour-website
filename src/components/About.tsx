4de8edf7b868a0261b29502aa021da1b8595da56
Bren <brendan@weiwei.wifi-res.warwick.ac.uk>
2021-11-16 17:50:57 +0000

    improved mobile friendliness and fixed bugs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import './About.css'
 import { ParallaxBanner } from 'react-scroll-parallax';
 
+import "tailwindcss/tailwind.css"
+
 
 import p1 from '../images/parallax-1.png';
 import p2 from '../images/parallax-2.png';
@@ -25,36 +27,38 @@ class About extends React.Component {
             <>
                 <Element name="about" className="element"></Element>
                 <div className='about'>
-                    <h1>ABOUT</h1>
-                    <p>Welcome to the University of Warwick Labour Society! (Warwick Labour for short). As a society we’re committed to the aims and values of the Labour Party, to Socialism and the Liberation of all people, we fight to keep Labour true to this.
+                    <h1 className='p-3 md:p-10'>ABOUT</h1>
+                    <p className='text-sm md:text-2xl p-3 md:p-10'>Welcome to the University of Warwick Labour Society! (Warwick Labour for short). As a society we’re committed to the aims and values of the Labour Party, to Socialism and the Liberation of all people, we fight to keep Labour true to this.
 
                         We’re one of the of the most active political societies in the country, holding regular events, talks and socials and campaigning heavily both on campus and throughout Warwickshire, Leamington, Coventry, Nuneaton — and further afield too.
 
                         We’re very friendly and love welcoming new members! So check out our Facebook page and our Facebook Discussion Group for all the latest updates, insights and to see how to get involved.</p>
                 </div>
-                <ParallaxBanner
-                    layers={[
-                        {
-                            expanded: false,
-                            image: p1,
-                            amount: 0.1,
-                        },
-                        {
-                            expanded: false,
-                            image: p2,
-                            amount: 0.3,
-                        },
-                        {
-                            expanded: false,
-                            image: p3,
-                            amount: 0.5,
-                        },
-                    ]}
-                    style={{
-                        height: "800px",
-                    }}
-                >
-                </ParallaxBanner>
+                <div className='hidden sm:h-1/3 sm:block'>
+                    <ParallaxBanner
+                        layers={[
+                            {
+                                expanded: false,
+                                image: p1,
+                                amount: 0.1,
+                            },
+                            {
+                                expanded: false,
+                                image: p2,
+                                amount: 0.3,
+                            },
+                            {
+                                expanded: false,
+                                image: p3,
+                                amount: 0.5,
+                            },
+                        ]}
+                        style={{
+                            height: "800px",
+                        }}
+                    >
+                    </ParallaxBanner>
+                </div>
             </>
 
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,13 +17,13 @@ class Header extends React.Component {
         return (
             <div>
 
-                <div className={`nav-sticky`}>
+                <div className={`nav-sticky text-sm sm:text-2xl`}>
 
 
-                    <Link activeClass="active" to="home" spy={true} smooth={true} offset={-100} duration={750} >
+                    <Link activeClass="active" to="home" spy={true} smooth={true} offset={-120} duration={750} >
                         <a className={`nav-title hover-underline-animation`}> HOME</a>
                     </Link>
-                    <Link activeClass="active" to="about" spy={true} smooth={true} offset={-100} duration={750} >
+                    <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={750} >
                         <a className='nav-title hover-underline-animation'>ABOUT</a>
                     </Link>
                     <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-100} duration={750} >
