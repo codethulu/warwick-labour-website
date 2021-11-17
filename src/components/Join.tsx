f709d51f35a1d4905d534ba109f511d2280aaf1e
codethulu <brendanbell2001@hotmail.com>
2021-11-17 12:52:01 +0000

    edited join us section

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -22,7 +22,7 @@ class Join extends React.Component {
                 <div className='join'>
 
                     <h1 className='scaled-title'>WANT TO GET INVOLVED?</h1>
-                    <p>Joining the society is the easiest way to keep up with what the society is doing. Doing so is <strong>FREE</strong> through the SU! Join to get involved in our campaigns, our socials to meet other left wing students on campus, and the wider labour community in the Warwick/Coventry area!</p>
+                    <p>Joining the society is the easiest way to keep up with what the society is doing. Doing so is <strong>FREE</strong> through the SU! Join to get involved in our campaigns, our socials to meet other left wing students on campus, and the wider labour community in the Warwick/Coventry area! The society is great for listening to exclusive speakers and getting involved in campaigns that matter and getting real political experience.</p>
                     <a href='https://www.warwicksu.com/shop/product/1785/'><div className='join-button'></div></a>
 
                     <div className='social-container'>
@@ -31,8 +31,8 @@ class Join extends React.Component {
                         <a href='http://instagram.com/warwicklabour'><div className='social-button instagram'></div></a>
                         <a href='https://discord.gg/YyfVtdZQKM'><div className='social-button discord'></div></a>
                     </div>
-                    <p>We also post our events on our socials, so please consider following them to get convenient updates and news!</p>
-                    <p>Any other questions, issues or suggestions? You are welcome to message us on any of our social media platforms listed above, or you can email us!</p>
+                    <p>We also post our events on our socials, so please consider following them to get convenient updates and news!
+                        Any other questions, issues or suggestions? You are welcome to message us on any of our social media platforms listed above, or you can email us!</p>
                     <p>General Inquiries: <a href="mailto: labourwarwick@gmail.com">labourwarwick@gmail.com</a></p>
                     <p>Technical Issues: <a href="mailto: brendan.bell@warwick.ac.uk">brendan.bell@warwick.ac.uk</a></p>
 
