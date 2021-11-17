4d8f9477da4c3542751d64c63014007ebb180467
codethulu <brendanbell2001@hotmail.com>
2021-11-17 12:38:58 +0000

    added email contact section

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -24,14 +24,18 @@ class Join extends React.Component {
                     <h1 className='scaled-title'>WANT TO GET INVOLVED?</h1>
                     <p>Joining the society is the easiest way to keep up with what the society is doing. Doing so is <strong>FREE</strong> through the SU! Join to get involved in our campaigns, our socials to meet other left wing students on campus, and the wider labour community in the Warwick/Coventry area!</p>
                     <a href='https://www.warwicksu.com/shop/product/1785/'><div className='join-button'></div></a>
-                    <p>We also post our events on our socials, so please consider following them to get convenient updates and news!</p>
+
                     <div className='social-container'>
                         <a href='https://www.facebook.com/WarwickLabour/'><div className='social-button facebook'></div></a>
                         <a href='https://twitter.com/warwicklabour'><div className='social-button twitter'></div></a>
                         <a href='http://instagram.com/warwicklabour'><div className='social-button instagram'></div></a>
                         <a href='https://discord.gg/YyfVtdZQKM'><div className='social-button discord'></div></a>
-
                     </div>
+                    <p>We also post our events on our socials, so please consider following them to get convenient updates and news!</p>
+                    <p>Any other questions, issues or suggestions? You are welcome to message us on any of our social media platforms listed above, or you can email us!</p>
+                    <p>General Inquiries: <a href="mailto: labourwarwick@gmail.com">labourwarwick@gmail.com</a></p>
+                    <p>Technical Issues: <a href="mailto: brendan.bell@warwick.ac.uk">brendan.bell@warwick.ac.uk</a></p>
+
 
                 </div>
             </>
