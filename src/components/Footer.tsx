bb64441c2020a3dc526a6ef51460f4233e14bfe4
MP0415 <matthewprice911@gmail.com>
2023-03-25 21:42:49 +0000

    Changes to Join page and Footer that I noticed
    Discord is now defunct (afaik), added new Tech Officer email also to the website.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
         return (
 
             <div className='footer'>
-                <p>Website designed and written by <a href="https://github.com/codethulu">codethulu</a>. (Brendan Bell)</p>
+                <p>Website initially designed and written by <a href="https://github.com/codethulu">codethulu</a>. (Brendan Bell) for Waarwick Labour. Currently maintained by the <a href="https://github.com/MP0415">23/24 Tech Officer (Matthew Price)</a>.</p>
 
             </div>
 
--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -29,7 +29,6 @@ class Join extends React.Component {
                         <a className='social-link' href='http://instagram.com/warwicklabour'><h3>INSTAGRAM</h3></a>
                         <a className='social-link' href='https://www.facebook.com/WarwickLabour/'><h3>FACEBOOK</h3></a>
                         <a className='social-link' href='https://twitter.com/warwicklabour'><h3>TWITTER</h3></a>
-                        {/* <a className='social-link' href='https://discord.gg/YyfVtdZQKM'><h3>DISCORD</h3></a> */}
                     </div>
 
                     {/* <div className='social-container'>
@@ -44,7 +43,7 @@ class Join extends React.Component {
                 <div className='divider' />
                 <div className='container'>
                     <p>General Inquiries: <a className='email-link' href="mailto: labourwarwick@gmail.com">labourwarwick@gmail.com</a></p>
-                    <p>Technical Issues: <a className='email-link' href="mailto: brendan@codethulu.dev">brendan@codethulu.dev</a></p>
+                    <p>Technical Issues: <a className='email-link' href="mailto: Matthew.J.Price@warwick.ac.uk">Matthew.J.Price@warwick.ac.uk</a></p>
 
 
                 </div>
